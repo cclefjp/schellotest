08d3397dbb09819fe11a690182e037967ef04541
cclef <cclefjp@outlook.com>
2020-11-14 11:22:07 +0900

    エコーバック処理を統合

--- schellotest/schellotest.php
+++ schellotest/schellotest.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: schellotest
-Plugin URI: https://notes.cclef-dev.net/posts/160
+Plugin URI: https://notes.cclef-dev.net/posts/174
 Description: simple shortcode test plugin
-Version: 0.2
+Version: 1.0
 Author: Cclef Development
 Author URI: https://notes.cclef-dev.net/
 License: GPLv3
@@ -24,13 +24,25 @@ You should have received a copy of the GNU General Public
 License along with schellotest.
 */
 
-/* ショートコード処理の本体 */
+/* ショートコード部に挿入されるフォーム出力の記載 */
 function schello_func($atts) {
-    $schello =  'この文章は';
-    $schello = $schello .  'schelloショートコードにより出力されました。';
-    return esc_html($schello);
-}
 
+    if (array_key_exists('echoback', $_POST)) {
+       
+        $schello_pre = 'schelloショートコードによりユーザーが入力した文章は ';
+        $schello_user = $_POST['echoback'];
+        $schello_post = ' です。';
+
+        return sanitize_text_field($schello_pre . $schello_user . $schello_post);
+
+    } else {
+        $formstatement = '<form action="' . get_permalink() . '" method="post">
+                    エコーバック: <input type="text" name="echoback" />
+                    <input type="submit" />
+                    </form>';
+        return $formstatement;
+    }
+}
 
 /* [schello]ショートコードを登録する */
 add_shortcode('schello', 'schello_func');
